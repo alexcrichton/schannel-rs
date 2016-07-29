6b8788b9862348087a70b99b5bb3b8ed41e2b235
Alex Crichton <alex@alexcrichton.com>
2016-07-28 17:36:48 -0700

    Implement accepting schannel connections
    THis commit updates the various bindings here and there to be able to accept TLS
    connections as well as being able to initiate them. The tricky part here is
    dealing with certificates, but it looks like Windows at least supports importing
    a PKCS#12 certificate (like OSX) and then clients just need to figure out how to
    trust it.
    
    A test has been added for this functionality, but it currently requires
    modifying the root certificate store, so instructions have been added to the
    README as well.

--- src/cert_store.rs
+++ src/cert_store.rs
@@ -1,6 +1,11 @@
+//! Bindings to winapi's certificate-store related APIs.
+
 use crypt32;
-use std::io;
+use std::ffi::OsStr;
 use std::fmt;
+use std::io;
+use std::mem;
+use std::os::windows::prelude::*;
 use std::ptr;
 use winapi;
 
@@ -9,6 +14,7 @@ use ctl_context::CtlContext;
 
 use Inner;
 
+/// Representation of certificate store on Windows, wrapping a `HCERTSTORE`.
 pub struct CertStore(winapi::HCERTSTORE);
 
 unsafe impl Sync for CertStore {}
@@ -49,6 +55,8 @@ impl Inner<winapi::HCERTSTORE> for CertStore {
 }
 
 impl CertStore {
+    /// Creates a new in-memory certificate store which certificates and CTLs
+    /// can be added to.
     pub fn memory() -> io::Result<Memory> {
         unsafe {
             let store = crypt32::CertOpenStore(winapi::CERT_STORE_PROV_MEMORY as winapi::LPCSTR,
@@ -63,11 +71,144 @@ impl CertStore {
             }
         }
     }
+
+    /// Opens up the specified key store within the context of the current user.
+    ///
+    /// Known valid values for `which` are "Root" and "My".
+    pub fn open_current_user(which: &str) -> io::Result<CertStore> {
+        unsafe {
+            let data = OsStr::new(which)
+                             .encode_wide()
+                             .chain(Some(0))
+                             .collect::<Vec<_>>();
+            let store = crypt32::CertOpenStore(winapi::CERT_STORE_PROV_SYSTEM_W as winapi::LPCSTR,
+                                               0,
+                                               0,
+                                               winapi::CERT_SYSTEM_STORE_CURRENT_USER,
+                                               data.as_ptr() as *mut _);
+            if store.is_null() {
+                Err(io::Error::last_os_error())
+            } else {
+                Ok(CertStore(store))
+            }
+        }
+    }
+
+    /// Opens up the specified key store within the context of the local
+    /// machine.
+    ///
+    /// Known valid values for `which` are "Root" and "My".
+    pub fn open_local_machine(which: &str) -> io::Result<CertStore> {
+        unsafe {
+            let data = OsStr::new(which)
+                             .encode_wide()
+                             .chain(Some(0))
+                             .collect::<Vec<_>>();
+            let store = crypt32::CertOpenStore(winapi::CERT_STORE_PROV_SYSTEM_W as winapi::LPCSTR,
+                                               0,
+                                               0,
+                                               winapi::CERT_SYSTEM_STORE_LOCAL_MACHINE,
+                                               data.as_ptr() as *mut _);
+            if store.is_null() {
+                Err(io::Error::last_os_error())
+            } else {
+                Ok(CertStore(store))
+            }
+        }
+    }
+
+    /// Imports a PKCS#12-encoded key/certificate pair, returned as a
+    /// `CertStore` instance.
+    ///
+    /// The password must also be provided to decrypt the encoded data.
+    pub fn import_pkcs12(data: &[u8], password: &str) -> io::Result<CertStore> {
+        unsafe {
+            let mut blob = winapi::CRYPT_INTEGER_BLOB {
+                cbData: data.len() as winapi::DWORD,
+                pbData: data.as_ptr() as *mut u8,
+            };
+            let password = OsStr::new(password)
+                                 .encode_wide()
+                                 .chain(Some(0))
+                                 .collect::<Vec<_>>();
+            let res = crypt32::PFXImportCertStore(&mut blob,
+                                                  password.as_ptr(),
+                                                  0);
+            if res.is_null() {
+                Err(io::Error::last_os_error())
+            } else {
+                Ok(CertStore(res))
+            }
+        }
+    }
+
+    /// Returns an iterator over the certificates in this certificate store.
+    pub fn certs(&mut self) -> Certs {
+        Certs { store: self, cur: None }
+    }
+
+    /// Adds a certificate context to this store.
+    ///
+    /// This function will add the certificate specified in `cx` to this store.
+    /// A copy of the added certificate is returned.
+    pub fn add_cert(&mut self, cx: &CertContext) -> io::Result<CertContext> {
+        unsafe {
+            let flags = winapi::CERT_STORE_ADD_REPLACE_EXISTING;
+            let mut ret = ptr::null();
+            let res = crypt32::CertAddCertificateContextToStore(self.0,
+                                                                cx.as_inner(),
+                                                                flags,
+                                                                &mut ret);
+            if res != winapi::TRUE {
+                Err(io::Error::last_os_error())
+            } else {
+                Ok(CertContext::from_inner(ret))
+            }
+        }
+    }
 }
 
+/// An iterator over the certificates contained in a `CertStore`, returned by
+/// `CertStore::iter`
+pub struct Certs<'a> {
+    store: &'a mut CertStore,
+    cur: Option<CertContext>,
+}
+
+impl<'a> Iterator for Certs<'a> {
+    type Item = CertContext;
+
+    fn next(&mut self) -> Option<CertContext> {
+        unsafe {
+            let cur = self.cur.take().map(|p| {
+                let ptr = p.as_inner();
+                mem::forget(p);
+                ptr
+            });
+            let cur = cur.unwrap_or(ptr::null_mut());
+            let next = crypt32::CertEnumCertificatesInStore(self.store.0, cur);
+
+            if next.is_null() {
+                self.cur = None;
+                None
+            } else {
+                let next = CertContext::from_inner(next);
+                self.cur = Some(next.clone());
+                Some(next)
+            }
+        }
+    }
+}
+
+/// An in-memory store of certificates and CTLs, created by `CertStore::memory`
+/// and can be converted into a `CertStore`.
+#[derive(Clone)]
 pub struct Memory(CertStore);
 
 impl Memory {
+    /// Adds a new certificate to this memory store.
+    ///
+    /// For example the bytes could be a DER-encoded certificate.
     pub fn add_encoded_certificate(&mut self, cert: &[u8]) -> io::Result<CertContext> {
         unsafe {
             let mut cert_context = ptr::null();
@@ -87,6 +228,9 @@ impl Memory {
         }
     }
 
+    /// Adds a new CTL to this memory store, in its encoded form.
+    ///
+    /// This can be created through the `ctl_context::Builder` type.
     pub fn add_encoded_ctl(&mut self, ctl: &[u8]) -> io::Result<CtlContext> {
         unsafe {
             let mut ctl_context = ptr::null();
@@ -106,25 +250,20 @@ impl Memory {
         }
     }
 
+    /// Consumes this memory store, returning the underlying `CertStore`.
     pub fn into_store(self) -> CertStore {
         self.0
     }
 }
 
-#[cfg(test)]
-mod test {
-    use std::fs::File;
-    use std::io::Read;
-
-    use super::*;
-
-    #[test]
-    fn load() {
-        let mut file = File::open(concat!(env!("CARGO_MANIFEST_DIR"), "/test/cert.der")).unwrap();
-        let mut cert = vec![];
-        file.read_to_end(&mut cert).unwrap();
-
-        let mut store = CertStore::memory().unwrap();
-        store.add_encoded_certificate(&cert).unwrap();
-    }
-}
+// #[cfg(test)]
+// mod test {
+//     use super::*;
+//
+//     #[test]
+//     fn load() {
+//         let cert = include_bytes!("../test/localhost.der");
+//         let mut store = CertStore::memory().unwrap();
+//         store.add_encoded_certificate(cert).unwrap();
+//     }
+// }
